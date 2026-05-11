09f9934d6daf29563006223e0006b67e980068b5
kohumukini <kealii.ohumukini@gmail.com>
2026-05-11 11:37:23 -0700

    First commit

--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,40 @@
 public class Practice {
     public static void main(String[] args) {
-        
+        ListNode myNode = new ListNode(); 
+        myNode.data = 'x';
+
+        ListNode anotherNode = new ListNode(); 
+        anotherNode.data = 't';
+        myNode.next = anotherNode;
+
+        ListNode moreNode = new ListNode(); 
+        moreNode.data = 'r'; 
+        anotherNode.next = moreNode; 
+
+        ListNode coolNode = new ListNode(); 
+        coolNode.data = 'w'; 
+
+        coolNode.next = myNode; 
+
+        ListNode extra = moreNode;
+        moreNode.data = 'e'; 
+
+        ListNode hello = new ListNode(); 
+        hello.data = 'x'; 
+        extra.next = hello;
+
+        printList(coolNode);
+    }
+
+    public static void printList(ListNode head) {
+        // Starting from head
+        // print all data in list in order
+
+        ListNode current = head; 
+
+        while (current != null) {
+            System.out.println(current.data);
+            current = current.next;  
+        }
     }
 }
